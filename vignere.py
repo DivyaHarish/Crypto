66460c1085fcd330948203ca3010466239957193
DivyaHarish <divyaharishr@gmail.com>
2020-03-29 15:00:29 +0530

    Encryption and decryption with known key(Vignere)

--- vignere.py
+++ vignere.py
@@ -1,9 +1,10 @@
-LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ"    #key space
+
+#conditioning of the plain text and function calls (encrypt and decrypt)
 def main():
     key = "PIZZA"
-#    key = "Z"
     msg = "COMMON SENSE IS NOT SO COMMON"
-#    msg = "S"
+
 
     msg=msg.strip().replace(" ","")
     print(msg)
@@ -13,17 +14,17 @@ def main():
     plain = ''.join(decrypt(key,cipher))
     print(plain)
 
+#encrypt function accepts a key and message to encrypt the message using VIgnere polyalphabetic cipher
 def encrypt(key,msg):
     encrypt_msg = []
     for i in range(0,len(msg)):
         num1=(LETTERS.find(key[i%len(key)]))
-        #print(num1)
         num2=(LETTERS.find(msg[i]))
-        #print(num2)
         num = (num1 + num2)%26 #(LETTERS.find(key[i%len(key)]) + LETTERS.find(msg[i])) % 26
         encrypt_msg.append(LETTERS[num])
     return encrypt_msg
 
+#decrypt function operates on the cipher text and calculates the plain text provided the key is known
 def decrypt(key,msg):
     decrypt_msg = []
     for i in range(0,len(msg)):
